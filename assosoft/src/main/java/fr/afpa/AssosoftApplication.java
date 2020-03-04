0e7545cdb51aa6fcf8bf8877cf1b7e3078a902b0
Eric <mrpanam.p@gmail.com>
2020-03-04 15:31:21 +0100

    1st push assosoft

---
+++ assosoft/src/main/java/fr/afpa/AssosoftApplication.java
@@ -0,0 +1,13 @@
+package fr.afpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssosoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssosoftApplication.class, args);
+	}
+
+}
---
+++ assosoft/src/test/java/fr/afpa/AssosoftApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.afpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssosoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
